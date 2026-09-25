6a8c5963362b3a26499d6d004ab9d0bec87a27f0
Isis noh <isis.noh5075@alumnos.udg.mx>
2026-09-24 19:34:35 -0600

    print edad

---
+++ practicas/practica1.py
@@ -0,0 +1,20 @@
+
+edad = 15
+edad = edad + 1
+print(edad)
+
+
+IVA = 0.16 
+print (IVA)
+
+nombre = "Ana"
+edad = 15
+ciudad = "Guadalajara"
+print(nombre, edad, ciudad)
+
+IVA = 0.16
+precio = 100
+total = precio + (precio * IVA)
+print(total)    
+
+print (type (edad))
